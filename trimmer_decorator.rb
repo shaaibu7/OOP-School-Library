5daf0ba0ce9f91b770eed53375c7abe3990fe01e
shaaibu <shaaibusuleiman@gamil.com>
2023-04-18 17:31:57 +0100

    Fix trimmer method

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require './base_decorator'
 class Trimmer < Decorator
   def correct_name
     super()
-    @nameable.correct_name.length > 10 ? @nameable.correct_name[1...10] : @nameable.correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0...10] : @nameable.correct_name
   end
 end
